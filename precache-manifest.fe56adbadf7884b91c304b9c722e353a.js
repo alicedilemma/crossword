cf663c37701ff3eed66fe11faa9dd33cfb6e96ca
mgatland <mgatland@gmail>
2019-12-31 12:21:43 +1300

    Updates

---
+++ precache-manifest.fe56adbadf7884b91c304b9c722e353a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2c2fcc49852c23096e4c4bb2aa2132",
+    "url": "/crossword/index.html"
+  },
+  {
+    "revision": "3fc3951af9ff77a464d1",
+    "url": "/crossword/static/js/2.5c85dede.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/crossword/static/js/2.5c85dede.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09eedb786d85418544b7",
+    "url": "/crossword/static/js/main.7d4f72e2.chunk.js"
+  },
+  {
+    "revision": "3b993eca13560184c273",
+    "url": "/crossword/static/js/runtime-main.61952a52.js"
+  }
+]);
